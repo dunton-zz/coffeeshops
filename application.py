e5ffe363cac202dda7f3b026a4cb2a0b3c258051
Ryan Dunton <ryandunton1@gmail.com>
2016-06-24 19:49:03 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,23 +1,20 @@
 import os.path
 
 from catalog import app
-from catalog.database_setup import create_db
+from catalog.database_setup import create_database
 from catalog.populate_database import database_populator
 
 if __name__ == '__main__':
-    # App configuration
-    app.config['DATABASE_URL'] = 'sqlite:///coffeeshopmenu.db'
+    app.config['DATABASE_URL'] = 'sqlite:////var/www/coffeeshops/catalog/coffee$
     app.config['UPLOAD_FOLDER'] = '/vagrant/catalog/item_images'
     app.config['OAUTH_SECRETS'] = ''
-
-    app.secret_key = 'super_secret_key'  # This needs changing in production env
-
-    if app.config['DATABASE_URL'] == 'sqlite:///coffeeshopmenu.db':
-        if os.path.isfile('sqlite:///coffeeshopmenu.db') is False:
-            create_db(app.config['DATABASE_URL'])
+    
+    if app.config['DATABASE_URL'] == 'sqlite:////var/www/coffeeshops/catalog/co$
+        if os.path.isfile('sqlite:////var/www/coffeeshops/catalog/coffeeshopmen$
+            create_database('sqlite:////var/www/coffeeshops/catalog/coffeeshopm$
             populate_database()
-    else:  
-        create_database(app.config['DATABASE_URL'])
+    else:
+        create_database()
         populate_database()
 
     app.debug = True
