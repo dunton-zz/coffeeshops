5f81e3d147fcfc7a93629cb86045357d69b970ec
Ryan Dunton <ryandunton1@gmail.com>
2016-06-24 21:24:09 -0400

    Update and rename application.py to app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from catalog.database_setup import create_database
 from catalog.populate_database import database_populator
 
 if __name__ == '__main__':
-    app.config['DATABASE_URL'] = 'sqlite:////var/www/coffeeshops/catalog/coffee$
+    app.config['DATABASE_URL'] = 'sqlite:////var/www/coffeeshops/catalog/coffeeshopmenu.db'
     app.config['UPLOAD_FOLDER'] = '/vagrant/catalog/item_images'
     app.config['OAUTH_SECRETS'] = ''
     
-    if app.config['DATABASE_URL'] == 'sqlite:////var/www/coffeeshops/catalog/co$
-        if os.path.isfile('sqlite:////var/www/coffeeshops/catalog/coffeeshopmen$
-            create_database('sqlite:////var/www/coffeeshops/catalog/coffeeshopm$
+    if app.config['DATABASE_URL'] == 'sqlite:////var/www/coffeeshops/catalog/coffeeshopmenu.db'
+        if os.path.isfile('sqlite:////var/www/coffeeshops/catalog/coffeeshopmenu.db'):
+            create_database('sqlite:////var/www/coffeeshops/catalog/coffeeshopmenu.db')
             populate_database()
     else:
         create_database()
